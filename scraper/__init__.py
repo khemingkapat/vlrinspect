e4e6735dcb2bd8fd980ebcce3a44806c6cf13cf7
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-08-11 15:17:18 +0700

    add method

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,5 +1,9 @@
-from .scraper import get_upcoming_matches
+from scraper.history import get_team_history
+from .team import get_teams_from_match
+from .upcoming import get_upcoming_matches
 
 
 class Scraper:
     get_upcoming_matches = staticmethod(get_upcoming_matches)
+    get_teams_from_match = staticmethod(get_teams_from_match)
+    get_team_history = staticmethod(get_team_history)
