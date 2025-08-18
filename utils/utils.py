df0ec2dc1769ae9f400f7039fcea0c9ebabc0617
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-08-18 13:31:05 +0700

    move extract function to history, adding detect_type for detect string value

--- utils/utils.py
+++ utils/utils.py
@@ -23,61 +23,24 @@ re_strip = lambda sp, st: sp.join(
 #     return ""
 
 
-def extract_df_from_html(html: str, map_name: str) -> pd.DataFrame:
-    tree = HTMLParser(html)
-    side_list = ["All", "Attack", "Defense"]
-    headers = [th.text(strip=True) for th in tree.css("table thead tr th")][2:]
+def detect_type(value: str):
+    if value.lower() in {"none", "null", "nil"}:
+        return None
 
-    side_stat_cols = []
-    for side in side_list:
-        for header in headers:
-            side_stat_cols.append(f"{header}_{side}")
+    if value.lower() in {"true", "false"}:
+        return value.lower() == "true"
 
-    all_columns = ["Map", "Team", "Name", "Agent"] + side_stat_cols
+    try:
+        return int(value)
+    except ValueError:
+        pass
 
-    data_rows = []
+    try:
+        return float(value)
+    except ValueError:
+        pass
 
-    for tr in tree.css("table tbody tr"):
-        name = tr.css_first("div.text-of").text(strip=True)
-        team = tr.css_first("div.ge-text-light").text(strip=True)
-        agent_elem = tr.css_first("img")
-        agent = agent_elem.attributes.get("alt", None) if agent_elem else None
+    if "%" in value:
+        return int(value.replace("%", "")) / 100
 
-        row_data = {
-            "Map": map_name,
-            "Team": team,
-            "Name": name,
-            "Agent": agent,
-        }
-
-        for idx, td in enumerate(tr.css("td:not(.mod-player):not(.mod-agents)")):
-            if idx >= len(headers):
-                break
-
-            spans = td.css("span")
-            col = headers[idx]
-
-            for span in spans:
-                classes = span.attributes.get("class", "")
-                if not classes:
-                    continue
-
-                side = None
-                if "mod-both" in classes:
-                    side = "All"
-                elif "mod-t" in classes:
-                    side = "Attack"
-                elif "mod-ct" in classes:
-                    side = "Defense"
-
-                if side:
-                    column_name = f"{col}_{side}"
-                    row_data[column_name] = span.text(strip=True)
-
-        data_rows.append(row_data)
-
-    result = pd.DataFrame(data_rows, columns=all_columns)
-
-    result = result.set_index(["Map", "Team", "Name"])
-
-    return result
+    return value
