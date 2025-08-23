7553e1e5daf6578d7e9d443b3175de37b7a9f39c
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-08-23 11:40:12 +0700

    import every function as staticmethod

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,4 +1,11 @@
-from scraper.history import get_team_history_list, scrape_matches, scrape_match_info
+from scraper.history import (
+    get_team_history_list,
+    scrape_matches,
+    scrape_match_info,
+    extract_overview_from_html,
+    extract_round_result_from_html,
+    extract_economy_from_html,
+)
 from .team import get_teams_from_match
 from .upcoming import get_upcoming_matches
 import requests
@@ -9,6 +16,9 @@ class Scraper:
     get_teams_from_match = staticmethod(get_teams_from_match)
     get_team_history_list = staticmethod(get_team_history_list)
     scrape_match_info = staticmethod(scrape_match_info)
+    extract_economy_from_html = staticmethod(extract_economy_from_html)
+    extract_round_result_from_html = staticmethod(extract_round_result_from_html)
+    extract_df_from_html = staticmethod(extract_overview_from_html)
 
     @staticmethod
     def get_team_history(
@@ -18,15 +28,24 @@ class Scraper:
         url: str = "https://www.vlr.gg/",
     ):
         team1, team2 = get_teams_from_match(_session, match_url)
-        team1_hist_list, team_1_abbr = get_team_history_list(_session, team1, head=head)
-        team2_hist_list, team_2_abbr = get_team_history_list(_session, team2, head=head)
-        # print(team1_hist_list)
-        # print(team2_hist_list)
+        team1_hist_list, team1_abbr = get_team_history_list(_session, team1)
+        team2_hist_list, team2_abbr = get_team_history_list(_session, team2)
 
-        team_abbr = team_1_abbr | team_2_abbr
-        # print(team_abbr)
+        team_abbr = team1_abbr | team2_abbr
 
-        team1_hist = scrape_matches(_session, team1_hist_list, team_abbr)
-        team2_hist = scrape_matches(_session, team2_hist_list, team_abbr)
-        print(team1_hist)
-        print(team2_hist)
+        team1_hist = scrape_matches(
+            _session,
+            matches_url=team1_hist_list,
+            head=head,
+            team_abbreviate=team_abbr,
+            full_name=list(team1_abbr.keys())[0],
+        )
+        team2_hist = scrape_matches(
+            _session,
+            matches_url=team2_hist_list,
+            head=head,
+            team_abbreviate=team_abbr,
+            full_name=list(team2_abbr.keys())[0],
+        )
+
+        return team1_hist, team2_hist
