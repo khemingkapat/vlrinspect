e21d6de73fc1407df2764c3c1c8c5f4ccc28adc0
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-09-05 21:40:58 +0700

    add team win condition plot

--- src/visualizer/__init__.py
+++ src/visualizer/__init__.py
@@ -2,12 +2,19 @@ from .logic.overall import (
     get_team_win_lose,
     get_team_buy_type_win_lose,
     get_player_stats,
+    get_team_win_condition,
 )
-from .overall import plot_team_win_lose, plot_player_stats, plot_team_buy_type_win_lose
+from .overall import (
+    plot_team_win_condition,
+    plot_team_win_lose,
+    plot_player_stats,
+    plot_team_buy_type_win_lose,
+)
 
 
 class Visualizer:
 
+    # overall
     get_team_win_lose = staticmethod(get_team_win_lose)
     plot_team_win_lose = staticmethod(plot_team_win_lose)
 
@@ -16,3 +23,6 @@ class Visualizer:
 
     get_team_buy_type_win_lose = staticmethod(get_team_buy_type_win_lose)
     plot_team_buy_type_win_lose = staticmethod(plot_team_buy_type_win_lose)
+
+    get_team_win_condition = staticmethod(get_team_win_condition)
+    plot_team_win_condition = staticmethod(plot_team_win_condition)
